49d8901bc773b9cd7ef4f315bfec6f93d1daf07a
JeonIlju <lanace93@gmail.com>
2019-10-21 11:37:35 +0900

    add some features

--- src/App.js
+++ src/App.js
@@ -11,16 +11,106 @@ function App() {
   const onFetching = () => {
     return new Promise((resolve, reject) => {
       setTimeout(function() {
-        resolve(1)
+        reject(1);
       }, 3000);
-    })
+    });
+  }
+
+  const onFetchingList = () => {
+    const promise1 = new Promise((resolve, reject) => {
+      console.log('promise1');
+      setTimeout(function() {
+        console.log('promise1 - resolve');
+        resolve(1);
+      }, 3000);
+    });
+
+    const promise2 = new Promise((resolve, reject) => {
+      console.log('promise2');
+      setTimeout(function() {
+        console.log('promise2 - resolve');
+        reject(2);
+      }, 5000);
+    });
+
+    const promise3 = new Promise((resolve, reject) => {
+      console.log('promise3');
+      setTimeout(function() {
+        console.log('promise3 - resolve');
+        resolve(3);
+      }, 1000);
+    });
+
+
+    return [promise1, promise2, promise3];
+  }
+
+  const onStateChanged = (state) => {
+    console.log('onStateChanged', state)
+  }
+
+  const onError = (error) => {
+    console.error('onError', error);
+  }
+
+  const onSuccess = (response) => {
+    console.log('onSuccess', response);
+  }
+
+  const onFail = (response) => {
+    console.warn('onFail', response);
+  }
+
+  const onProcess = (process) => {
+    console.log('onProcess', process);
   }
 
   return (
     <div className="App">
       <h1>react-state-button example!</h1>
 
-      <PendingButton onFetching={onFetching} timeout={5000}>test</PendingButton>
+      <h2>Basic </h2>
+
+      <PendingButton 
+        timeout={5000}
+        fetchingText='Please wait...'
+        successText='Success fetching'
+        failText='Fail fetching'
+        fetchMode='sequence'
+        onStateChanged={onStateChanged}
+        onFetching={onFetchingList}
+        onError={onError}
+        onSuccess={onSuccess}
+        onFail={onFail}
+        onProcess={onProcess}>
+          <div>
+            <p>
+              Pending Button Test
+            </p>
+          </div>
+      </PendingButton>
+
+      <h2>Multi Fetching</h2>
+
+      <PendingButton 
+        timeout={5000}
+        fetchingText='Please wait...'
+        successText='Success fetching'
+        failText='Fail fetching'
+        fetchMode='sequence'
+        onStateChanged={onStateChanged}
+        onFetching={onFetchingList}
+        onError={onError}
+        onSuccess={onSuccess}
+        onFail={onFail}
+        onProcess={onProcess}>
+          <div>
+            <p>
+              Multi fetching Pending Button
+            </p>
+          </div>
+      </PendingButton>
+
       <TriButton>test</TriButton>
       <TriCheckbox>test</TriCheckbox>
     </div>
--- src/components/PendingButton.jsx
+++ src/components/PendingButton.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class PendingButton extends React.Component {
 
@@ -6,42 +7,120 @@ class PendingButton extends React.Component {
     super(props);
 
     this.state = {
-      isPending: false
+      isPending: false,
+      text: ''
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.isPending !== this.state.isPending) {
+      if (this.props.onStateChanged) {
+        this.props.onStateChanged(this.state.isPending);
+      }
     }
   }
 
   handleClicked = () => {
+    console.log('clicked!');
+
     if (this.state.isPending) {
       console.log('clicked already');
     } else {
-      this.setState({isPending: true});
+      this.setState({
+        isPending: true,
+        text: this.props.fetchingText
+      });
 
-      this.props.onFetching().then(data => {
-        console.log(data);
-        this.setState({isPending: false});
-      }).catch(error => {
-        console.error('error on fetching', error);
-        this.setState({isPending: false});
-      })
+      this.fetchProcess();
     }
+  }
+
+  fetchProcess = () => {
+    if (!this.props.onFetching) {
+      // NOTE: fetch가 없는 경우
+      console.warn('onFetching is undefind. please set fetching props. you must return promise by array or function');
+      return;
+    }
+
+    let fetchingList = [];
+
+    if (this.props.onFetching instanceof Function) {
+      fetchingList = [this.props.onFetching()];
+    } else {
+      fetchingList = fetchingList.concat(this.props.onFetching.map(fetch => fetch()));
+    }
+
+    let result;
+    if (this.props.fetchMode === 'inconsecutive') {
+      result = Promise.all(fetchingList)
+    } else {
+      // result = sequence(fetchingList);
+      result = fetchingList.reduce((prev, next) => {
+        console.log(prev, next);
+        return prev.then(() => {
+          console.log('inner', next);
+          return next;
+        })
+      }, Promise.resolve());
+    }
+
+    result.then(data => {
+      console.log('fetching success', data);
+      this.setState({
+        isPending: false,
+        text: this.props.successText
+      });
+
+      if (this.props.onSuccess) {
+        this.props.onSuccess(data);
+      }
+    }).catch(error => {
+      console.error('fetching fail', error);
+      this.setState({
+        isPending: false,
+        text: this.props.failText
+      });
+
+      if (this.props.onFail) {
+        this.props.onFail(error);
+      }
+    });
   }
 
   render() {
     console.log('render()', this.props);
 
-    let pendingText = '';
-
-    if (this.state.isPending) {
-      pendingText = 'pending';
-    }
-
     return (
       <button onClick={this.handleClicked}>
-        {pendingText}
+        {this.state.text}
         {this.props.children}
       </button>
     )
   }
+}
+
+PendingButton.propTypes = {
+  timeout: PropTypes.number,
+  fetchingText: PropTypes.string,
+  successText: PropTypes.string,
+  failText: PropTypes.string,
+  fetchMode: PropTypes.oneOf([
+    'sequence', 'inconsecutive'
+  ]),
+
+  onStateChanged: PropTypes.func,
+  onFetching: PropTypes.oneOfType([
+    PropTypes.array,
+    PropTypes.func
+  ]),
+  onError: PropTypes.func,
+  onSuccess: PropTypes.func,
+  onFail: PropTypes.func,
+  onProcess: PropTypes.func
+}
+
+PendingButton.defaultProps = {
+  fetchMode: 'sequence'
 }
 
 export default PendingButton;
