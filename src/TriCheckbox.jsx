4845dcdbf58799ad8e13230e9f602401cf0b7c5f
JeonIlju <lanace93@gmail.com>
2019-11-01 09:40:18 +0900

    fixed maximum func call in onChange bug

--- src/TriCheckbox.jsx
+++ src/TriCheckbox.jsx
@@ -12,12 +12,15 @@ class TriCheckBox extends React.Component {
     React.Children.forEach(props.children, (child, index) => {
       if (isMuiElement(child, ['TriCheckBox'])) {
         const childProps = Object.assign({}, child.props);
+        childProps.onMonkyChange = childProps.onChange;
         childProps.onChange = (value) => {
           if (this.props.propagation) {
             this.onChildChangeCheckBox(index, value);
           }
 
-          return props.onChange(value);
+          if (childProps.onMonkyChange) {
+            childProps.onMonkyChange(value);
+          }
         }
         childrenNodes.push(childProps);
       }
@@ -59,13 +62,25 @@ class TriCheckBox extends React.Component {
     }, () => {
       const checkedCount = this.state.childrenNodes.filter(child => child.checked).length;
       if (checkedCount === 0) {
-        this.setState({value: 0});
+        this.onValueChange(0);
       } else if (checkedCount === this.state.childrenNodes.length) {
-        this.setState({value: 1});
+        this.onValueChange(1);
       } else {
-        this.setState({value: 2});
+        this.onValueChange(2);
       }
     });
+  }
+
+  onValueChange = (value) => {
+    if (value === this.state.value) {
+      return;
+    }
+
+    this.setState({value}, () =>{
+      if (this.props.onChange) {
+        this.props.onChange(value);
+      }
+    })
   }
 
   componentDidUpdate (prevProps) {
