297e8ffee3251e853222a339436c3b4cbf53ed6f
JeonIlju <lanace93@gmail.com>
2019-11-08 15:22:25 +0900

    add resizable story

--- stories/10-Container.stories.js
+++ stories/10-Container.stories.js
@@ -1,20 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { action } from '@storybook/addon-actions';
-import { LnFlexContainer, LnFlexPannel } from '../src/index'
+import { LnFlexContainer, LnResizableContainer, LnFlexPannel } from '../src/index'
 
 export default {
   title: 'Container'
 }
 
+export const LnResizableContainerSample = () => {
+
+  const [width, setWidht] = useState(100);
+  const [height, setHeight] = useState(100);
+
+  const onResize = (width, height) => {
+    setWidht(width);
+    setHeight(height);
+  }
+
+  return (
+    <LnResizableContainer width={width} height={height} onResize={onResize}>
+      <h1>Let's Resize!!!</h1>
+      <p>OH~~~~~~</p>
+      <button>recommend!</button>
+    </LnResizableContainer>
+  )
+}
+
 export const LnFlexContainerSample = () => {
+  const pannelProps1 = {
+    width: 320,
+    height: 320
+  }
+
+  const pannelProps2 = {
+    width: 100,
+    height: 200
+  }
+
+  const pannelProps3 = {
+    width: 620,
+    height: 320
+  }
+
+  const pannelProps4 = {
+    width: 420,
+    height: 120
+  }
 
   return (
     <LnFlexContainer>
-      <LnFlexPannel>
+      <LnFlexPannel {...pannelProps1}>
         <h1>Pannel Number 1</h1>
       </LnFlexPannel>
 
-      <LnFlexPannel>
+      <LnFlexPannel {...pannelProps2}>
         <h1>Pannel Number 2</h1>
         <div>
           <p>I am a boy!</p>
@@ -22,12 +60,12 @@ export const LnFlexContainerSample = () => {
         </div>
       </LnFlexPannel>
 
-      <LnFlexPannel>
+      <LnFlexPannel {...pannelProps3}>
         <h1>Pannel Number 3</h1>
         <input type="text"/>
       </LnFlexPannel>
 
-      <LnFlexPannel>
+      <LnFlexPannel {...pannelProps4}>
         <h1>Pannel Number 4</h1>
         <button>Submit!!</button>
       </LnFlexPannel>
